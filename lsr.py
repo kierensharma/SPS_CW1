c775864cac0622abe46116a8de58c5af4dd59181
kierensharma <kieren.sharma@gmail.com>
2020-03-02 14:19:19 +0000

    Added CSVs and implemented plotting function.

--- lsr.py
+++ lsr.py
@@ -13,3 +13,32 @@ def load_points_from_file(filename):
     """
     points = pd.read_csv(filename, header=None)
     return points[0].values, points[1].values
+
+def view_data_segments(xs, ys):
+    """Visualises the input file with each segment plotted in a different colour.
+    Args:
+        xs : List/array-like of x co-ordinates.
+        ys : List/array-like of y co-ordinates.
+    Returns:
+        None
+    """
+    assert len(xs) == len(ys)
+    assert len(xs) % 20 == 0
+    len_data = len(xs)
+    num_segments = len_data // 20
+    colour = np.concatenate([[i] * 20 for i in range(num_segments)])
+    plt.set_cmap('Dark2')
+    plt.scatter(xs, ys, c=colour)
+    plt.show()
+
+csv_file = sys.argv[1]
+line_segments = load_points_from_file(csv_file)
+
+x_coordinates = line_segments[0]
+y_coordinates = line_segments[1]
+
+if len(sys.argv) == 3 and sys.argv[2] == '--plot':
+    view_data_segments(x_coordinates, y_coordinates)
+    pass
+else:
+    pass
